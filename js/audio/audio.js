748fe5c1bf510431dec6a610daf0d687e6a043cc
exwayz <exwayz@users.noreply.github.com>
2026-08-08 17:41:55 +0800

    fix: default audio off, only on when user enables it

--- js/audio/audio.js
+++ js/audio/audio.js
@@ -2,7 +2,7 @@ const LS_SFX_VOL = "wa-nd-sfx-vol";
 const LS_SFX_ENABLED = "wa-nd-sfx-enabled";
 
 let _sfxVolume = parseFloat(localStorage.getItem(LS_SFX_VOL)) || 1;
-let _sfxEnabled = localStorage.getItem(LS_SFX_ENABLED) !== "0";
+let _sfxEnabled = localStorage.getItem(LS_SFX_ENABLED) === "1";
 
 function audio(path) {
   const a = new Audio(path);
